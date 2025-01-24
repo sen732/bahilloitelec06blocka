e38d2c1bf7f8e2c4cfb216b16b92fd4b119e247f
sen732 <kelvin.bahillo09@gmail.com>
2025-01-24 20:55:37 +0800

    bahillo

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './post/create-post/post.create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/post/create-post/post.create.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+@Component({
+    selector: 'post-create',
+    templateUrl: './post.create.component.html',
+    styleUrls: ['./post.create.component.css'],
+})
+export class PostCreateComponent {
+
+}
